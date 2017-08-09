3b3b1dc2c26a351f4a9c66545fbb2d30d95c59bb
kendall <motesmass@gmail.com>
2017-08-09 16:44:11 -0600

    added all layouts to link from home page, header, and footer. nothing is really added to those pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root  'pages#home'
-  get  '/home', to: 'pages#home'
+  get  '/home',    to: 'pages#home'
   get  '/contact', to: 'pages#contact'
+  get  '/about',   to: 'pages#about'
+  get  '/news', to: 'pages#news'
+  get  '/login', to: 'pages#login'
   get  '/signup',  to: 'users#new'
 
 
