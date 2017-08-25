1aa86eadf4bd6d74dc85aea3741ff0d82ff8406c
kendall <motesmass@gmail.com>
2017-08-25 15:06:29 -0700

    updated the password with bcrypt gem next thing to do is start on 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811015449) do
+ActiveRecord::Schema.define(version: 20170825214428) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
